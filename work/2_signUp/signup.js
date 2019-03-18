0a31c71969ccbf80dd28eaa3add498e51639c203
ndemia <n.demia@tuta.io>
2019-03-18 19:33:08 +0100

    add new log in project

--- work/2_signUp/signup.js
+++ work/2_signUp/signup.js
@@ -1,23 +1,26 @@
 const navSignUp = document.getElementById('nav__signUp');
 const navLogIn = document.getElementById('nav__logIn');
-const mainSignUp = document.getElementById('access--signUp');
-const mainLogIn = document.getElementById('access--logIn');
+const mainSignUp = document.getElementById('user-access__sign-up-view');
+const mainLogIn = document.getElementById('user-access__log-in-view');
+const userAccess = document.getElementById('user-access');
 
 navLogIn.onclick = function showLogIn() {
-	navSignUp.classList.replace('nav__active', 'nav__inactive');
-	navLogIn.classList.replace('nav__inactive', 'nav__active');
-	navLogIn.classList.remove('nav--log--inactive');
-	navSignUp.classList.add('nav--sign--inactive');
-	mainSignUp.classList.replace('main__active', 'main__inactive');
-	mainLogIn.classList.replace('main__inactive', 'main__active');
+	userAccess.classList.add('user-access--new-height');
+	navSignUp.classList.replace('nav--active', 'nav--inactive');
+	navLogIn.classList.replace('nav--inactive', 'nav--active');
+	navLogIn.classList.remove('nav__logIn--inactive-shadow');
+	navSignUp.classList.add('nav__signUp--inactive-shadow');
+	mainSignUp.classList.replace('user-access--active', 'user-access--inactive');
+	mainLogIn.classList.replace('user-access--inactive', 'user-access--active');
 }
 
 navSignUp.onclick = function showSignUp() {
-	navLogIn.classList.replace('nav__active', 'nav__inactive');
-	navSignUp.classList.replace('nav__inactive', 'nav__active');
-	navSignUp.classList.remove('nav--sign--inactive');
-	navLogIn.classList.add('nav--log--inactive');
-	mainLogIn.classList.replace('main__active', 'main__inactive');
-	mainSignUp.classList.replace('main__inactive', 'main__active');
+	userAccess.classList.remove('user-access--new-height');
+	navLogIn.classList.replace('nav--active', 'nav--inactive');
+	navSignUp.classList.replace('nav--inactive', 'nav--active');
+	navSignUp.classList.remove('nav__signUp--inactive-shadow');
+	navLogIn.classList.add('nav__logIn--inactive-shadow');
+	mainLogIn.classList.replace('user-access--active', 'user-access--inactive');
+	mainSignUp.classList.replace('user-access--inactive', 'user-access--active');
 }
 
