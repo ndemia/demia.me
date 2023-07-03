231f7f978dea879a20e04a9463b59a8a54a0dee0
ndemia <ndemia@fastmail.com>
2023-07-03 23:09:38 +0200

    Maintenance mode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 import { checkDarkMode } from './checkDarkMode.js';
 import { updateBooksAndMusic } from './updateBooksAndMusic.js';
 
+const emailButton = document.querySelector('.js-button-email');
+const message = document.querySelector('.message');
+let clicks = 0;
+
+const showMessage = (messageType, clicks) => {
+	if (messageType === 'success' && clicks == 1) {
+		message.innerText = `E-mail copied!`;
+		message.classList.add('show');
+		setTimeout(() => {
+			message.innerText = ``;
+			message.classList.remove('show');
+		}, 3000);
+	} else if (messageType === 'success' && clicks == 2) {
+		message.innerText = `Again?! Copied. There won't be a next time.`;
+		message.classList.add('show');
+		setTimeout(() => {
+			message.innerText = ``;
+			message.classList.remove('show');
+		}, 3000);
+	} else if (messageType === 'success' && clicks == 3) {
+		emailButton.classList.add('fall');
+		setTimeout(() => {
+			emailButton.classList.add('hide');
+		}, 2000);
+	} else {
+		message.innerText = `Hmmm, my e-mail was not copied. Better try again.`;
+	}
+};
+
+const copyEmailToClipboard = async (clicks) => {
+	try {
+		await navigator.clipboard.writeText('ndemia@fastmail.com');
+		showMessage('success', clicks);
+	} catch (error) {
+		showMessage('fail');
+	}
+};
+
+emailButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	clicks++;
+	copyEmailToClipboard(clicks);
+});
+
 document.addEventListener('DOMContentLoaded', checkDarkMode());
-document.addEventListener('DOMContentLoaded', updateBooksAndMusic());
