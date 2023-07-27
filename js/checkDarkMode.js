00c54a1a17d2466582698f93d7a18ce95a780f38
ndemia <ndemia@fastmail.com>
2023-07-27 14:54:05 +0200

    Apply new version of the website

--- js/checkDarkMode.js
+++ js/checkDarkMode.js
@@ -1,11 +1,13 @@
 // Change favicon according to theme
 export const checkDarkMode = function () {
-
-	// In case browser's dark theme is enabled, returns boolean
+	// In case browser's dark theme is enabled. Returns boolean
 	let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-	// If dark true, show white favicon
-	if (isDark) {
+	// Check for DOS mode as it has its own favicon
+	let isDos = document.documentElement.classList.contains('dos-theme');
+
+	// If dark true, and there's no DOS mode, show white favicon
+	if (isDark === true && isDos === false) {
 		document.querySelector('link[type="image/svg+xml"]').href = 'favicon-white.svg';
 	}
 };
--- js/updateBooksAndMusic.js
+++ js/updateBooksAndMusic.js
@@ -1,18 +1,16 @@
 // Get book and music information to display in the footer
 export const updateBooksAndMusic = function () {
-
 	fetch('js/currently.json')
-	.then((response) => {
-		return response.json();
-	})
-	.then(data => {
-		document.querySelector('.book').innerText = data.book;
-		document.querySelector('.author').innerText = data.author;
-		document.querySelector('.band').innerText = data.band;
-		document.querySelector('.album').innerText = data.album;
-	})	
-	.catch((err) => {
-		console.log(err);
-	});
-
-}
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			document.querySelector('.book').innerText = data.book;
+			document.querySelector('.author').innerText = data.author;
+			document.querySelector('.band').innerText = data.band;
+			document.querySelector('.album').innerText = data.album;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
